4e7dbd88f750074f4a1fc9864601ae38052912c1
patnz <114084721+patnz@users.noreply.github.com>
2023-04-05 11:26:01 +1200

    changed localhost urls

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -2,27 +2,25 @@ import request from 'superagent'
 import * as Models from '../../common/models'
 
 export function apiGetBooks(): Promise<Models.Book[]> {
-  return request
-    .get(`http://localhost:3000/api/v1`)
-    .then((response) => response.body)
+  return request.get(`/api/v1`).then((response) => response.body)
 }
 
 export function apiAddBook(book: Models.Book): Promise<Models.Book[]> {
   return request
-    .post(`http://localhost:3000/api/v1`)
+    .post(`/api/v1`)
     .send(book)
     .then((response) => response.body)
 }
 
 export function apiDeleteBook(id: number): Promise<number> {
   return request
-    .delete(`http://localhost:3000/api/v1/delete/${id}`)
+    .delete(`/api/v1/delete/${id}`)
     .then((deletedBookId) => deletedBookId.body)
 }
 
 export function apiEditBook(book: Models.Book): Promise<Models.Book[]> {
   return request
-    .patch(`http://localhost:3000/api/v1/edit/`)
+    .patch(`/api/v1/edit/`)
     .send(book)
     .then((res) => res.body)
 }
